12afed3026a1463c11864e6f89715d0f27933818
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-07-21 13:32:53 +0200

    init

---
+++ src/main/java/com/pg/pet_grooming/PetGroomingApplication.java
@@ -0,0 +1,13 @@
+package com.pg.pet_grooming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetGroomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetGroomingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pg/pet_grooming/PetGroomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.pet_grooming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetGroomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
