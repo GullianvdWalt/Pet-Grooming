23422d61e8d99a0f8b963f095972cd5985546b40
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-07-22 14:24:05 +0200

    Created Employee Repository, Service and Controller classes

---
+++ src/main/java/com/pg/pet_grooming/Services/EmployeeService.java
@@ -0,0 +1,19 @@
+/*
+ * This is the Employee Service Class
+ * This Class implements the Pet Owner Reposistory 
+ * The controller sends request to this class
+ */
+package com.pg.pet_grooming.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pg.pet_grooming.Repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {  
+    
+    // Inject EmployeeRepository
+    @Autowired
+    private EmployeeRepository employeeRepository;
+}
