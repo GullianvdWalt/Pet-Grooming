c9fff760096ea1badbb03f92fc93382de5a4b7f0
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-08-31 11:44:46 +0200

    Updated Views

--- src/main/java/com/pg/pet_grooming/Controllers/AppointmentController.java
+++ src/main/java/com/pg/pet_grooming/Controllers/AppointmentController.java
@@ -10,12 +10,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppointmentController {
     
-    @GetMapping("/newAppointments")
+    // Main New Appointment Page
+    @RequestMapping("/newAppointments")
     public String newAppointment(Model model){
+      // Set Page Title
+      String pageTitle = "New Appointment";
+      model.addAttribute("pageTitle", pageTitle);
+      // Set Page Title Icon
+      String iconUrl = "newAppointment.png";
+      model.addAttribute("iconUrl", iconUrl);
         return "NewAppointment";
     }
+    
+
 }
