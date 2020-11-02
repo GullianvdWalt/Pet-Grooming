427aec9e11064af2c425ddb82bed5d7ee391c9ad
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-11-02 07:24:35 +0200

    Test Branch

--- src/main/java/com/pg/pet_grooming/Controllers/ProfitController.java
+++ src/main/java/com/pg/pet_grooming/Controllers/ProfitController.java
@@ -241,7 +241,7 @@ public class ProfitController {
         // Set Page Title Icon
         String iconUrl = "exspenseReport.png";
         model.addAttribute("iconUrl", iconUrl);
-         // Test
+         // Test c
         return "Profit";
     }
     // Get Profit By Day
